9de685279a9559960715a4042f714729eae30d59
rueschan <rescalante21@hotmail.com>
2017-09-25 20:42:30 -0500

    Cast a matriz de personas

--- Proyecto.py
+++ Proyecto.py
@@ -1,9 +1,14 @@
-import cognitive_face as CF
+import detect_faces, info_analizer
 
-KEY = '4c9d869cdd3a4adba127a880088daac4' # Replace with a valid subscription key (keeping the quotes in place).
-CF.Key.set(KEY)
+def main():
+    foto1 = "http://vignette2.wikia.nocookie.net/vikingstv/images/7/78/Katheryn_Winnick.png/revision/latest?cb=20130405200728"
+    foto2 = "http://warp.la/wp-content/uploads/2017/06/the-killers-2-e1485012869982.jpg"
+    jsonFoto1 = "{'url':'" + foto1 + "'}"
+    jsonFoto2 = "{'url':'" + foto2 + "'}"
 
-# You can use this example JPG or replace the URL below with your own URL to a JPEG image.
-img_url = 'https://raw.githubusercontent.com/Microsoft/Cognitive-Face-Windows/master/Data/detection1.jpg'
-result = CF.face.detect(img_url)
-print result
+    infoPhoto1 = detect_faces.readFace(jsonFoto1)
+    infoPhoto2 = detect_faces.readFace(jsonFoto2)
+
+    info_analizer.compare(infoPhoto1, infoPhoto2)
+
+main()
--- detect_faces.py
+++ detect_faces.py
@@ -1,4 +1,4 @@
-import httplib, urllib, base64, json
+import httplib, urllib, base64, json, cognitive_face
 
 ###############################################
 #### Update or verify the following values. ###
@@ -26,33 +26,14 @@ headers = {
 # Request parameters.
 params = urllib.urlencode({
     'returnFaceId': 'true',
-    'returnFaceLandmarks': 'false',
-    'returnFaceAttributes': 'age,gender,headPose,smile,facialHair,glasses,emotion,hair,makeup,occlusion,accessories,blur,exposure,noise',
+    'returnFaceLandmarks': 'true',
+    'returnFaceAttributes': 'age,gender,smile,facialHair,hair',
+    #'age,gender,headPose,smile,facialHair,glasses,emotion,hair,makeup,occlusion,accessories,blur,exposure,noise'
 })
 
-# The URL of a JPEG image to analyze.
-body = "{'url':'https://upload.wikimedia.org/wikipedia/commons/c/c3/RH_Louise_Lillian_Gish.jpg'}"
-
-try:
-    # Execute the REST API call and get the response.
-    conn = httplib.HTTPSConnection('westcentralus.api.cognitive.microsoft.com')
-    conn.request("POST", "/face/v1.0/detect?%s" % params, body, headers)
-    response = conn.getresponse()
-    data = response.read()
-
-    # 'data' contains the JSON data. The following formats the JSON data for display.
-    parsed = json.loads(data)
-    print ("Response:")
-    print (json.dumps(parsed, sort_keys=True, indent=2))
-    conn.close()
-
-except Exception as e:
-    print("[Errno {0}] {1}".format(e.errno, e.strerror))
-
 # Method for reading faces
-def readFace():
-    # The URL of a JPEG image to analyze.
-    body = "{'url':'https://upload.wikimedia.org/wikipedia/commons/c/c3/RH_Louise_Lillian_Gish.jpg'}"
+def readFace(body):
+    result = ''
 
     try:
         # Execute the REST API call and get the response.
@@ -63,9 +44,14 @@ def readFace():
 
         # 'data' contains the JSON data. The following formats the JSON data for display.
         parsed = json.loads(data)
-        print ("Response:")
-        print (json.dumps(parsed, sort_keys=True, indent=2))
+        # print ("Response:")
+        # print (json.dumps(parsed, sort_keys=True, indent=2))
+
+        result = (json.dumps(parsed, sort_keys=True, indent=2))
+
         conn.close()
 
     except Exception as e:
         print("[Errno {0}] {1}".format(e.errno, e.strerror))
+
+    return result
