ecf537321bb82cf43bfc27d881c7e3dab9727d38
Santiago <A01371036@itesm.mx>
2017-09-26 18:35:52 -0500

    Se reconoce video y guarda frames

--- video_frames.py
+++ video_frames.py
@@ -2,11 +2,9 @@ import argparse
 import cv2
 
 vidcap = cv2.VideoCapture('C:\Users\Santino\Cmanai\SemanaiNKOTB\Videos\Video.mp4')
-success, image = vidcap.read()
-count = 0;
-while success:
-    success, image = vidcap.read()
-    cv2.imwrite("frame%d.jpg" % count, image)  # save frame as JPEG file
-    if cv2.waitKey(10) == 27:  # exit if Escape is hit
-        break
-    count += 1
+vidcap.set(cv2.CAP_PROP_POS_MSEC,20000)      # just cue to 20 sec. position
+success,image = vidcap.read()
+if success:
+    cv2.imwrite("frame20sec.jpg", image)     # save frame as JPEG file
+    cv2.imshow("20sec",image)
+    cv2.waitKey()
