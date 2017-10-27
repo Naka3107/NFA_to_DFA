bf8612cae47342b79054dcc261840fb1c6e67582
Santiago <A01371036@itesm.mx>
2017-10-27 10:24:17 -0500

    Se guarda en archivos

--- Proyecto.py
+++ Proyecto.py
@@ -1,35 +1,47 @@
 from itertools import *
+import os, inspect
 
 nodos = []
 estados = "{"
 
 def main():
     global nodos, estados
-    for linea in open('test','r').readlines():
-        nodes = set([])
-        symbols = set([])
-        flag = True
-        while flag:
-            if linea[-1] == "}":
-                flag = False
-                linea = linea[:-1]
-            linea = linea[1:]
-            nodos = new_nodes(linea)
-            for n in nodos:
-                symbols.add(n[0])
-                nodes.add(n[1])
-                nodes.add(n[2])
-            for c in list(powerset(nodes)):
-                print c
-                for sy in symbols:
-                    if (len(c) >= 1):
-                        estados += "(" + str(sy) + ",{"
-                        for comb in c:
-                            print comb
-                            estados += comb
-                        estados += "}," + str(new_transitions(c, sy)) + "),"
-            estados += "}"
-            print estados
+    PATH = os.path.dirname(os.path.abspath(inspect.getfile(inspect.currentframe())))
+    PATH = PATH.replace('\\', '/')
+    PATH = PATH + "/Textos/"
+    try:
+        for file in os.listdir(PATH):
+            if file.startswith("respuesta"):
+                os.remove(PATH+file)
+            elif file.endswith(".txt"):
+                nfa=PATH+file
+        for linea in open(nfa,'r').readlines():
+            nodes = set([])
+            symbols = set([])
+            flag = True
+            while flag:
+                if linea[-1] == "}":
+                    flag = False
+                    linea = linea[:-1]
+                linea = linea[1:]
+                nodos = new_nodes(linea)
+                for n in nodos:
+                    symbols.add(n[0])
+                    nodes.add(n[1])
+                    nodes.add(n[2])
+                for c in list(powerset(nodes)):
+                    for sy in symbols:
+                        if (len(c) >= 1):
+                            estados += "(" + str(sy) + ",{"
+                            for comb in c:
+                                estados += comb
+                            estados += "}," + str(new_transitions(c, sy)) + "),"
+                estados += "}"
+            f=open(PATH+'respuesta.txt','w')
+            f.write(estados)
+            print "El DFA se guardo con exito."
+    except NameError:
+        print "No se encontro el archivo"
 
 
 def new_nodes(linea):
@@ -77,3 +89,5 @@ def new_transitions(comb, sym):
 def powerset(set):
     l = list(set)
     return chain.from_iterable(combinations(l, r) for r in range(len(l) + 1))
+
+main()
